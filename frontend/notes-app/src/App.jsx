4ab3cce11426e9725713dcbf0f5d28680bd3ded4
AkhilReddyModugu <akhilreddymodugu13@gmail.com>
2024-09-10 02:17:29 +0530

    Minor changes

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -7,10 +7,10 @@ import Signup from './pages/SignUp/Signup';
 const routes= (
   <Router>
     <Routes>
-      <Route path="/" exact element={<Login />} />
       <Route path='/dashboard' exact element={<Home/>}/>
       <Route path='/login' exact element={<Login/>}/>
       <Route path='/signup' exact element={<Signup/>}/>
+      <Route path="/" element={<Login />} />
     </Routes>
   </Router>
 );
